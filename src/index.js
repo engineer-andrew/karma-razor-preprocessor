8cd0f2c4c5c51fca8d4832cb3e7508a89e996b5b
engineer-andrew <andrew.webster@rocketmail.com>
2016-11-15 20:59:04 -0700

    Initial check-in

---
+++ src/index.js
@@ -0,0 +1,65 @@
+function buildNewContents(content, lastEndIndex) {
+    var startIndex = content.indexOf('<script>', lastEndIndex);
+    var result = {};
+
+    if (startIndex > -1) {
+        var endIndex = content.indexOf('</script>', startIndex);
+
+        var script = content.substring(startIndex + 8, endIndex);
+        var remaining = content.substring(endIndex);
+
+        result =  {
+            endIndex: endIndex,
+            remaining: remaining,
+            script: script,
+        };
+    } else {
+        result.remaining = ''
+    }
+
+    return result;
+};
+
+var createRazorPreprocessor = function(config) {
+    config = typeof config === 'object' ? config : {};
+
+    var transformPath = config.transformPath || function(filePath) {
+        return filePath.replace(/\.cshtml$/, '.js');
+    };
+
+    return function(content, file, done) {
+        file.path = transformPath(file.originalPath);
+        
+        var newContent = '';
+        var endIndex = 0;
+        var contents = content;
+        var counter = 0;
+        while (true){
+            var result = buildNewContents(contents, endIndex);
+
+            endIndex = result.endIndex;
+            contents = result.remaining;
+            counter++;
+
+            if (counter > 10){
+                throw 'There are more than 10 embedded script statements in the file!'
+            }
+
+            if (result.remaining.length === 0) {
+                break;
+            } else {
+                newContent += result.script;
+            }
+        }
+
+        newContent = newContent.replace('@WriteJson(Model);', '{}');
+
+        done(newContent);
+    };
+};
+
+createRazorPreprocessor.$inject = ['config.razorPreprocessor'];
+
+module.exports = {
+    'preprocessor:razor': ['factory', createRazorPreprocessor]
+}
